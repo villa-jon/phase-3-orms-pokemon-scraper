6fd2ce5d951feeca257b98783c9d103cc4a381b1
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-10-03 01:23:19 -0700

    commit tree 83d8cf34c2e362671cf824453d11a0aa8b421156

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+	attr_accessor :id, :type, :name, :db
+
+	def initialize( id:, type:, name:, db:)
+		@id, @type, @name, @db = id, type, name, db
+	end
+
+	def self.save(name, type, db)
+		db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+	end 
+
+	def self.find(id_num, db)
+		ash_pokedex = db.execute("SELECT * FROM pokemon WHERE id = ?", id_num).flatten
+		Pokemon.new(id: ash_pokedex[0], name: ash_pokedex[1], type: ash_pokedex[2], db: db)
+	end 
+
 end
